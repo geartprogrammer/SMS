7eb69e4f4f95ba3f09dedb832a1980a027c120b2
geartgf2006 <geartprogrammer@gmail.com>
2020-01-31 15:17:17 +0000

    Initial commit

---
+++ code/main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('500x500')
+root.title("Registration Form")
+
+Button(root, text='Submit',width=20,bg='brown',fg='white',command=database).place(x=180,y=380)
+Button(root, text='Submit',width=20,bg='brown',fg='white',command=database).place(x=180,y=380)
+Button(root, text='Submit',width=20,bg='brown',fg='white',command=database).place(x=180,y=380)
+
+root.mainloop()
