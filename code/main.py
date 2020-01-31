1bb176a2ec3b5ad331a3e70b75d51bdb69ba9c97
Geart Ferhati <geartprogrammer@gmail.com>
2020-01-31 17:00:32 +0000

    Update code/main.html

--- code/main.py
+++
@@ -1,9 +0,0 @@
-<DOCTYPE html>
-<html>
-<head>
-    <title>School Managment System<title>
-    
-    <button type="button">Click Me!</button>
-    <button type="button">Click Me!</button>
-    
-</html>
