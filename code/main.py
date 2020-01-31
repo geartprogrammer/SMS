26c79243708afbc51a57a93f17b1182c7e560467
Geart Ferhati <geartprogrammer@gmail.com>
2020-01-31 15:36:07 +0000

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,11 +1,5 @@
-from tkinter import *
-
-root = Tk()
-root.geometry('500x500')
-root.title("Registration Form")
-
-Button(root, text='Submit',width=20,bg='brown',fg='white',command=database).place(x=180,y=380)
-Button(root, text='Submit',width=20,bg='brown',fg='white',command=database).place(x=180,y=380)
-Button(root, text='Submit',width=20,bg='brown',fg='white',command=database).place(x=180,y=380)
-
-root.mainloop()
+<DOCTYPE html>
+<html>
+<head>
+    <title>School Managment System<title>
+</html>
